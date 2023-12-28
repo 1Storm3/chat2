b600f1d8a888785650f5607b34a49f3bfdc0509d
1Storm3 <zemskoff2013@yandex.ru>
2023-12-28 18:34:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,9 @@ io.on("connection", (socket) => {
   });
 
   socket.on("sendMessage", ({ message, params }) => {
+    saveMessage(data)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
     const username = findUser(params);
     let time = moment().format("HH:mm");
     if (username) {
@@ -73,9 +76,6 @@ io.on("connection", (socket) => {
         data: { username, message, time },
       });
       const data = { username, message, time };
-      saveMessage(data)
-        .then((response) => console.log(response))
-        .catch((err) => console.log(err));
     }
   });
 
