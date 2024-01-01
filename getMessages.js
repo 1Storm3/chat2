d4f364b57e555bf45b031f569b70a35ec35ab33e
1Storm3 <zemskoff2013@yandex.ru>
2024-01-02 00:57:22 +0300

    Update getMessages.js

--- getMessages.js
+++ getMessages.js
@@ -17,7 +17,7 @@ const pool = new Pool({
 async function getMessages(room) {
   let newMessages = await pool.query(
     // `SELECT * FROM messages WHERE room = $1 ORDER BY timedata ASC LIMIT 100`,
-    `SELECT message, username, to_char(timedata, 'YYYY-MM-DD HH24:MI:SS') as timedata, room
+    `SELECT message, username, to_char(timedata, 'YYYY-MM-DD HH24:MI') as timedata, room
      FROM messages WHERE room = $1 ORDER BY timedata ASC LIMIT 100`,
     [room]
   );
