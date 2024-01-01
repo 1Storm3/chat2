95cf5b4c82297efad8265a408994eba7b7dd7575
1Storm3 <zemskoff2013@yandex.ru>
2024-01-01 20:35:00 +0300

    Update saveMessage.js

--- saveMessage.js
+++ saveMessage.js
@@ -17,14 +17,12 @@ const pool = new Pool({
 
 async function saveMessage(data) {
   const { message, time, user } = data;
-  // const currentDate = new Date().toISOString().slice(0, 10);
-  // const dateTime = `${currentDate}T${time}`;
-  const formattedTime = moment(`2000-01-01T${time}`).format(
-    "YYYY-MM-DD HH:mm:ss"
-  );
+  const currentDate = new Date().toISOString().slice(0, 10);
+  const dateTime = `${currentDate}T${time}`;
+
   await pool.query(
     "INSERT INTO messages (message, username, timedata, room) values ($1, $2, $3, $4) RETURNING *",
-    [message, user.name, formattedTime, user.room]
+    [message, user.name, dateTime, user.room]
   );
 }
 
