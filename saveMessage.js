cb2bba871d34afe00119420e0b8987f6812a5f2a
1Storm3 <zemskoff2013@yandex.ru>
2024-01-01 19:47:52 +0300

    Update saveMessage.js

--- saveMessage.js
+++ saveMessage.js
@@ -1,4 +1,5 @@
 const { Pool } = require("pg");
+const moment = require("moment");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -18,9 +19,17 @@ async function saveMessage(data) {
   const { message, time, user } = data;
   // const currentDate = new Date().toISOString().slice(0, 10);
   // const dateTime = `${currentDate}T${time}`;
+  // Получаем текущую дату
+  const currentDate = moment().format("YYYY-MM-DD");
+
+  // Форматируем переданное время в часах и минутах
+  const formattedTime = moment(`2000-01-01T${time}`).format("HH:mm");
+
+  // Создаем строку с датой и временем в нужном формате
+  const dateTime = `${currentDate} ${formattedTime}`;
   await pool.query(
     "INSERT INTO messages (message, username, timedata, room) values ($1, $2, $3, $4) RETURNING *",
-    [message, user.name, time, user.room]
+    [message, user.name, dateTime, user.room]
   );
 }
 
