bafd6759e35fcbe6aee125d8adbd09b32b2c7a9a
1Storm3 <zemskoff2013@yandex.ru>
2024-01-03 17:44:35 +0300

    м1

--- saveMessage.js
+++ saveMessage.js
@@ -1,18 +1,18 @@
 const { Pool } = require("pg");
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     require: true,
-//   },
-// });
-
 const pool = new Pool({
-  user: "storm",
-  host: "localhost",
-  database: "test",
-  password: "meepo2014",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+  },
 });
+
+// const pool = new Pool({
+//   user: "storm",
+//   host: "localhost",
+//   database: "test",
+//   password: "meepo2014",
+//   port: 5432,
+// });
 
 async function saveMessage(data) {
   const { message, time, user } = data;
