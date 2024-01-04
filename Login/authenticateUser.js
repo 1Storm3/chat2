cf0eb0015cad256e81960ec619f26bd5e0bfda97
1Storm3 <zemskoff2013@yandex.ru>
2024-01-04 15:53:08 +0300

    Update authenticateUser.js

--- Login/authenticateUser.js
+++ Login/authenticateUser.js
@@ -12,12 +12,12 @@ const pool = new Pool({
 async function authenticateUser(username, password) {
   const query = "SELECT * FROM users WHERE username = $1";
   const result = await pool.query(query, [username]);
-  const user = result.rows[0];
+
   const isAuthenticated =
     result.rows.length > 0 &&
     (await metautil.validatePassword(password, result.rows[0].password));
 
-  return { isAuthenticated, user };
+  return { isAuthenticated, user: result.rows[0] }, console.log(user);
 }
 
 module.exports = authenticateUser;
